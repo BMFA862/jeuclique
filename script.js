7d4870b3ecf4d4e322c0658eff6c7c67354dd2bf
BMFA862 <benjamin.moulron@gmail.com>
2026-05-26 15:27:46 +0200

    opacity-levelup

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const niveauAffichage = document.getElementById('niveau');
 const efficience = document.getElementById('efficience');
 const price = document.getElementById('price');
 const priceAffichage = document.getElementById('price-affichage');
+const txtLvl = document.getElementById('txtLvl');
+const txtEff = document.getElementById('txtEff');
 let score = 0;
 let niveau = 0;
 let efficienceScore = 1;
@@ -12,6 +14,17 @@ let efficienceScore = 1;
 monBouton.addEventListener('click', () => {
     score = score + 1 * (efficienceScore);
     scoreAffichage.textContent = score;
+    if (score >=10){
+        if (score >= 10 * efficienceScore*0.5) {
+            levelUp.style.opacity = 1;
+        }
+        else {
+            levelUp.style.opacity = 0;
+        }
+    }
+    else {
+        levelUp.style.opacity = 0;
+    }
 });
 
 
@@ -21,8 +34,15 @@ levelUp.addEventListener('click', () => {
         scoreAffichage.textContent = score;
         niveau = niveau + 1;
         niveauAffichage.textContent = niveau;
-        efficienceScore = efficienceScore + niveau*2;
+        efficienceScore = 2 ** niveau;
         efficience.textContent = `x${efficienceScore}`;
+        if (niveau >= 1) {
+            txtLvl.style.opacity = 1;
+            txtEff.style.opacity = 1;
+        }
+        if (score <= 10 * efficienceScore*0.5) {
+            levelUp.style.opacity = 0;
+        }
     }
 });
 
